4a27a6bf3551c84e4f789e4e2df973a36785d7aa
StefanFeederle <stefan.feederle@gmail.com>
2016-06-03 21:06:49 +0200

    new search element in header
    input gets focus on view load

--- app/app.js
+++ app/app.js
@@ -52,3 +52,20 @@ snippets.run( ['$rootScope', '$state', '$stateParams',
         $rootScope.$stateParams = $stateParams; 
     }
 ]);
+
+snippets.directive('focus', function($timeout) {
+    return {
+        scope : {
+            trigger : '@focus'
+        },
+        link : function(scope, element) {
+            scope.$watch('trigger', function(value) {
+                if (value === "true") {
+                    $timeout(function() {
+                        element[0].focus();
+                    });
+                }
+            });
+        }
+    };
+}); 

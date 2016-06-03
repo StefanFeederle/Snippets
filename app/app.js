425760aed52f7a9b8ce7ea865173116a2a4afb76
StefanFeederle <stefan.feederle@gmail.com>
2016-06-03 22:56:23 +0200

    new directive for material colors

--- app/app.js
+++ app/app.js
@@ -69,3 +69,19 @@ snippets.directive('focus', function($timeout) {
         }
     };
 }); 
+
+snippets.directive('pickmaterialcolor', function() {
+  return {
+    'scope': false,
+    'link': function(scope, element, attrs) {
+        var string = attrs.pickmaterialcolor;         
+        if(!string)return;
+        var materialColors = ["f44336", "E91E63", "9C27B0", "673AB7", "3F51B5", "2196F3", "03A9F4", "00BCD4", "009688", "4CAF50", "8BC34A", "CDDC39", "FFC107", "FF9800", "FF5722", "795548", "9E9E9E", "607D8B"];
+        var hash = 2;
+        for (var i = 0; i < string.length; i++) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+        element.css('background-color', '#'+materialColors[hash %= 18]);
+    }
+  };
+});
--- app/controllers/list.js
+++ app/controllers/list.js
@@ -9,18 +9,6 @@ snippets.controller("listController", function($scope, snippetsService, $state, 
     }
     getSnippets();
 
-    $scope.MaterialColorfromString = function(str){
-        if(!str)return;
-        var materialColors = ["f44336", "E91E63", "9C27B0", "673AB7", "3F51B5", "2196F3", "03A9F4", "00BCD4", "009688", "4CAF50", "8BC34A", "CDDC39", "FFC107", "FF9800", "FF5722", "795548", "9E9E9E", "607D8B"];
-        var hash = 2;
-        for (var i = 0; i < str.length; i++) {
-            hash = str.charCodeAt(i) + ((hash << 5) - hash);
-        }
-        return {
-            "background-color": '#'+materialColors[hash %= 18]
-        }
-    }
-
     $scope.selectSnippet = function(snippet){
         $state.go('home.snippet', {id: snippet.id});
     };
